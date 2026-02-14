e83ca5c2cd890ede1aa696aa819f66ccac2a06c0
DIPanjan198 <dipchoudhuri23@gmail.com>
2026-02-14 21:46:49 +0530

    my backend code

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ mongoose
 
 /* ================= SERVER ================= */
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
